976ac8fcf667575b961e5d69f318d50bdd8f3681
imeepos <imeepos@outlook.com>
2025-10-13 01:09:32 +0800

    fix: env

--- apps/web/src/environments/environment.prod.ts
+++ apps/web/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'http://43.240.223.138:3000/api',
   tokenKey: 'access_token',
   refreshTokenKey: 'refresh_token',
   timeout: 10000,
-  wsUrl: 'ws://localhost:3000',
+  wsUrl: 'ws://43.240.223.138:3000',
   wsNamespace: 'screens'
 };
