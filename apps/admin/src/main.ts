7b98bbf753bcf74b2139ca37ed7c616670df46d2
imeepos <imeepos@sker.com>
2025-10-14 05:32:36 +0000

    fix: bug

--- apps/admin/src/main.ts
+++ apps/admin/src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import localeZhHans from '@angular/common/locales/zh-Hans';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+
+registerLocaleData(localeZhHans, 'zh-CN');
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
