f216bd8690dd691da29ac15e3ccdef1feaee6b61
imeepos <imeepos@sker.com>
2025-10-21 08:20:24 +0000

    feat: fix bug

--- packages/utils/src/common.ts
+++ packages/utils/src/common.ts
@@ -21,7 +21,7 @@ export function debounce<T extends (...args: any[]) => any>(
   fn: T,
   delay: number
 ): (...args: Parameters<T>) => void {
-  let timeoutId: NodeJS.Timeout | null = null;
+  let timeoutId: ReturnType<typeof setTimeout> | null = null;
 
   return function (...args: Parameters<T>) {
     if (timeoutId) {
